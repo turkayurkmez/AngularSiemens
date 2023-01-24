c969a5740d3179425cfd16d95b85f6cc6885fe27
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-01-24 09:48:32 +0300

    component layouts completed

---
+++ projectTracker/src/app/project/project.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent {
+
+  @Input('currentProject') project:any; 
+
+}
